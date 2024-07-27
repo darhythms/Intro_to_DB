d62341e955f474bc06a9a4b0eeff2811a907ecf5
David Oladapo <ibksmart2004@gmail.com>
2024-07-27 16:11:29 +0100

    Update task_3.sql

--- task_3.sql
+++ task_3.sql
@@ -1,4 +1,2 @@
--- List all tables in the specified database
-SELECT TABLE_NAME 
-FROM INFORMATION_SCHEMA.TABLES 
-WHERE TABLE_SCHEMA = DATABASE();
+-- List all tables in the current database
+SHOW TABLES;
